66aa356f75b44741898085d1ceb1ceb3c6ae3542
Jeremy Leif <jeremy.leif@gmail.com>
2017-06-13 13:01:19 -0500

    added heroku-ized config var

--- server/routes/data.js
+++ server/routes/data.js
@@ -12,7 +12,15 @@ var pg = require('pg');
 //   max: 10,
 //   idleTimeoutMillis: 30000
 // };//end of config
-
+var config = {
+  user: process.env.PG_USER || null, //env var: PGUSER
+  password: process.env.DATABASE_SECRET || null, //env var: PGPASSWORD
+  host: process.env.DATABASE_SERVER || 'localhost', // Server hosting the postgres database
+  port: process.env.DATABASE_PORT || 5432, //env var: PGPORT
+  database: process.env.DATABASE_NAME || 'psi', //env var: PGDATABASE
+  max: 10, // max number of clients in the pool
+  idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+};
 //pool / pg constructor function
 var pool = new pg.Pool(config);
 
