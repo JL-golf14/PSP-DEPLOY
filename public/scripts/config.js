52bb538e399e7bb3568bf0f36a1d0d3801ffa898
Jeremy Leif <jeremy.leif@gmail.com>
2017-06-15 15:04:48 -0500

    updated src file ine firebase config

--- public/scripts/config.js
+++ public/scripts/config.js
@@ -1,12 +1,25 @@
-//CHRIS
-var config = {
-  apiKey: "AIzaSyD8F8SlJt302YSbjt_AS-C9HiwyGy4jbpg",
-  authDomain: "psp-msp.firebaseapp.com",
-  databaseURL: "https://psp-msp.firebaseio.com",
-  projectId: "psp-msp",
-  storageBucket: "psp-msp.appspot.com",
-  messagingSenderId: "135818172338"
-};
-
-firebase.initializeApp(config);
-//CHRIS
+// //CHRIS
+// var config = {
+//   apiKey: "AIzaSyD8F8SlJt302YSbjt_AS-C9HiwyGy4jbpg",
+//   authDomain: "psp-msp.firebaseapp.com",
+//   databaseURL: "https://psp-msp.firebaseio.com",
+//   projectId: "psp-msp",
+//   storageBucket: "psp-msp.appspot.com",
+//   messagingSenderId: "135818172338"
+// };
+//
+// firebase.initializeApp(config);
+// //CHRIS
+<script src="https://www.gstatic.com/firebasejs/4.1.2/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyD8F8SlJt302YSbjt_AS-C9HiwyGy4jbpg",
+    authDomain: "psp-msp.firebaseapp.com",
+    databaseURL: "https://psp-msp.firebaseio.com",
+    projectId: "psp-msp",
+    storageBucket: "psp-msp.appspot.com",
+    messagingSenderId: "135818172338"
+  };
+  firebase.initializeApp(config);
+</script
